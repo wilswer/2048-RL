1ee7f52748ff698b6aecc31c1108c201e8826b88
Wilhelm Söderkvist Vermelin <wilhelm.wermelin@icloud.com>
2022-01-01 22:02:45 +0100

    Fixed visualization of trained model

--- trained_dqn.py
+++ trained_dqn.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import torch as T
 from NumPy2048 import TerminalGame
@@ -8,27 +9,29 @@ def main():
     env = TerminalGame()
     agent = Agent(
         gamma=0.999,
-        epsilon=0.01,
+        epsilon=0.05,
         lr=0.00001,
         input_dims=(1, env.height, env.width),
         batch_size=128,
         n_actions=4,
         max_mem_size=500000,
         eps_decay=1e-6,
-        eps_end=0.00,
+        eps_end=0.05,
     )
     agent.Q_eval_target.load_state_dict(
         T.load('./saved_models/dqn_0_games')
     )
     agent.Q_eval_target.eval()
 
-    while True:
-        while not env.is_game_over():
-            env.draw_game()
-            observation = board_transform(env.board)
-            action = agent.choose_action(observation)
-            env.step(action)
-            env.draw_game()
+    env.draw_game()
+    observation = board_transform(env.board)
+    done = False
+    while not done:
+        action = agent.choose_action(observation)
+        observation, _, done = env.step(action)
+        observation = board_transform(observation)
+        time.sleep(0.2)
+        env.draw_game()
 
 
 if __name__ == '__main__':
